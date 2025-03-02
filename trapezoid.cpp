540b9be77fcaaf540acc7c44138c38d4c70a2ba4
wilfees <tenkovskaya_m@mail.ru>
2025-03-02 19:09:34 +0000

    a file with functions

---
+++ trapezoid.cpp
@@ -0,0 +1,9 @@
+double midLine(double a, double b){
+    return (a+b)/2.0;
+}
+double square(double a, double b, double h){
+    return ((a+b)/2.0)*h;
+}
+double perimetr(double a, double b, double c, double d){
+    return a+b+c+d;
+}
